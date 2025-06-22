9c732bd3200d087bc4ed84b9eae7552643c15680
Alvine Clive Munene <alvine.munene@student.moringaschool.com>
2025-06-22 23:41:21 +0300

    initial commit

--- server/app.py
+++ server/app.py
@@ -11,16 +11,55 @@ app.json.compact = False
 
 CORS(app)
 migrate = Migrate(app, db)
-
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+# GET 
+@app.route('/messages', methods=['GET'])
+def get_messages():
+    messages = Message.query.order_by(Message.created_at.asc()).all()
+    return jsonify([message.to_dict() for message in messages]), 200
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+# POST 
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.get_json()
+
+    try:
+        new_message = Message(
+            body=data['body'],
+            username=data['username']
+        )
+        db.session.add(new_message)
+        db.session.commit()
+        return jsonify(new_message.to_dict()), 201
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+    
+
+@app.route('/')
+def index():
+    return {'message': 'Flask is running!'}, 200
+
+
+# PATCH
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    message = Message.query.get_or_404(id)
+    data = request.get_json()
+
+    if 'body' in data:
+        message.body = data['body']
+        db.session.commit()
+
+    return jsonify(message.to_dict()), 200
+
+# DELETE
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = Message.query.get_or_404(id)
+    db.session.delete(message)
+    db.session.commit()
+    return '', 204
 
 if __name__ == '__main__':
     app.run(port=5555)
